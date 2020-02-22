0d68e509d948f6c91373302065d412ca1e63709d
Kiara <tycho01@pm.me>
2020-02-22 20:23:36 +0100

    build nix file

---
+++ nix/default.nix
@@ -0,0 +1,11 @@
+{ cudaVersion ? null }:
+let
+  shared = import nix/shared.nix { };
+in
+assert cudaVersion == null || cudaVersion == 9 || cudaVersion == 10;
+
+if cudaVersion == null
+then shared.shell-synthesis_cpu
+else if cudaVersion == 9
+  then shared.shell-synthesis_cudatoolkit_9_2
+  else shared.shell-synthesis_cudatoolkit_10_1
