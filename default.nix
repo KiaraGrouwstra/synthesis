6383daeed8881ad612bddb30adee326e95f83a49
Kiara Grouwstra <tycho01@pm.me>
2020-04-19 17:03:13 +0200

    default.nix: docs

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 let
   shared = import nix/shared.nix { };
 in
-  { inherit (shared) synthesis_cpu;
+  { inherit (shared) synthesis_cpu synthesis-docs;
     ${shared.nullIfDarwin "synthesis_cudatoolkit_9_2"} = shared.synthesis_cudatoolkit_9_2;
     ${shared.nullIfDarwin "synthesis_cudatoolkit_10_1"} = shared.synthesis_cudatoolkit_10_1;
   }
